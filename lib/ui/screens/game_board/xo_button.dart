06f7d40bedd99e666b0810c1e4114ec34a1c9639
Ahmed Nabil <ahmed.nabil@kazyon.com>
2025-12-15 20:16:22 +0200

    test commit2

--- lib/ui/screens/game_board/xo_button.dart
+++ lib/ui/screens/game_board/xo_button.dart
@@ -22,13 +22,17 @@ class XoButton extends StatelessWidget {
     }
     
     return Expanded(
-      child: InkWell(
-        onTap: (){
-          onClick(index);
-        },
-        child: symbol.isEmpty
-            ? Container()
-            : Image.asset( image),
+      child: Container(
+        color: Colors.transparent,
+        margin: EdgeInsets.all(8),
+        child: InkWell(
+          onTap: (){
+            onClick(index);
+          },
+          child: symbol.isEmpty
+              ? Container()
+              : Image.asset( image),
+        ),
       ),
     );
   }
