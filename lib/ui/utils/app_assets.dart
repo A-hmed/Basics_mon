30d7c751fb57b88c5330c8d3436d0efe0ae39635
Ahmed Nabil <ahmed.nabil@kazyon.com>
2025-12-15 19:52:48 +0200

    Add app_assets.dart in git

---
+++ lib/ui/utils/app_assets.dart
@@ -0,0 +1,5 @@
+abstract final class AppAssets{
+  static const icO = "assets/images/ic_o.png";
+  static const icX = "assets/images/ic_x.png";
+  static const introBg = "assets/images/xo_bg.png";
+}
